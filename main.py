48c05df5f3ab3589399ba7f6defc3f65b6cc02c6
damiandistefano <143121853+damiandistefano@users.noreply.github.com>
2026-03-15 22:33:01 -0300

    Agrego endpoint GET /api/v1/wells

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI(
     title="Oil & Gas Forecast API",
@@ -9,3 +9,14 @@ app = FastAPI(
 @app.get("/")
 def ruta_principal():
     return {"mensaje": "Hola equipo! El servidor de FastAPI está funcionando perfecto."}
+
+@app.get("/api/v1/wells")
+def obtener_pozos(
+    date_query: str = Query(..., description="Fecha para la cual se hace la consulta (YYYY-MM-DD)")
+):
+
+    return [
+        {"id_well": "POZO-001"},
+        {"id_well": "POZO-002"},
+        {"id_well": "POZO-003"}
+    ]
