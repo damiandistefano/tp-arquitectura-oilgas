b53a1159fa3399195ae23527b2e47d34486f52ea
joacoszterensus <jszterensus@udesa.edu.ar>
2026-04-20 20:29:00 -0300

    Mantengo counter http_requests_total al customizar instrumentator

--- app/api.py
+++ app/api.py
@@ -12,6 +12,8 @@ app = FastAPI(
 )
 
 Instrumentator(excluded_handlers=["/metrics"]).add(
+    metrics.requests()
+).add(
     metrics.latency(
         buckets=(0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1.0, 2.5, 5.0)
     )
